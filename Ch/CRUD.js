148d033e6741bb0745ce4dacaed9c4e89a2bace7
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-26 21:26:10 +0530

    crud operation in node js

---
+++ Ch/CRUD.js
@@ -0,0 +1,16 @@
+const fs=require("fs");
+
+// fs.mkdirSync("thapa");
+
+// fs.writeFileSync("thapa/bio.txt","yash varshney ");
+
+// fs.appendFileSync("thapa/bio.txt","starting again Node js");
+
+// const data=fs.readFileSync("thapa/bio.txt","utf8");
+// console.log(data);
+
+// fs.renameSync("thapa/bio.txt","thapa/io.txt");
+
+// fs.unlinkSync("thapa/io.txt");
+
+// fs.rmdirSync("thapa");
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
  fs.writeFileSync("readme.txt","my name is yash varshney");
 
-fs.appendFileSync("readme.txt"," today i study node js core modules");
+fs.appendFileSync("readme.txt"," today i study  node js core modules");
 
 
 //to rename the file name 
