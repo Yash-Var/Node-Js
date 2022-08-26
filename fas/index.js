f27ba939f68ed13f5ad3866288d9fccaa5f5f48d
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-26 22:16:39 +0530

    writeFile

--- fas/index.js
+++ fas/index.js
@@ -1,3 +1,7 @@
 const fs=require('fs');
 
-fs.writeFile('read.txt',"today is awesome day");
+fs.writeFile('read.txt',"today is awesome day",(err)=>{
+    console.log("file is created");
+    console.log(err);
+});
+
