e0251b5bc21ba07eeb9bdc4139d803c39b3e520b
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-27 20:32:11 +0530

    server

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,10 +1,17 @@
-const http=require('http');
+const http = require('http');
 
-const server=http.createServer((req,res)=>{
- res.end("hello from the others side thapa");
- 
+const server = http.createServer((req, res) => {
+    if(req.url=="/")
+    res.end("hello from the others side thapa");
+    else if(req.url=="/yash")
+    res.end("yash varshney")
+    else
+    {
+        res.writeHead(404,{"content-type":"text/html"});
+        res.end("<h1>404 erhhuhuhror</h1>")
+    }
 });
 
-server.listen(8000,"127.0.0.1",()=>{
-console.log("listening to the port no 8000");
+server.listen(8000, "127.0.0.1", () => {
+    console.log("listening to the port no 8000");
 });
