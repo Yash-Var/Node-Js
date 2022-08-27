0823c684b322d4ab895cd06bed5bb3bff3c51824
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-27 17:49:11 +0530

    import and export in node js

---
+++ ImportandExport/index.js
@@ -0,0 +1,3 @@
+
+const add=require('./operation');
+console.log(add(5,6));
---
+++ PathModule/path.js
@@ -0,0 +1,9 @@
+
+const path=require('path');
+
+console.log(path.extname('C:/Users/yashv/react/Node Js/PathModule/path.js'));
+
+
+console.log(path.parse('C:/Users/yashv/react/Node Js/PathModule/path.js'));
+const p=path.parse('C:/Users/yashv/react/Node Js/PathModule/path.js');
+console.log(p.name);
