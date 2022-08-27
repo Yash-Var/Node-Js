d9beaaae5e480b727ba15c41a71a14ebe9437399
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-27 20:08:54 +0530

    creating your own server

---
+++ httpServer/index.js
@@ -0,0 +1,10 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+ res.end("hello from the others side thapa");
+ 
+});
+
+server.listen(8000,"127.0.0.1",()=>{
+console.log("listening to the port no 8000");
+});
