b41b3c2bfce06ac6b2d813bfd6c9bf8f815926ec
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-27 11:32:51 +0530

    async CRUD opertion

---
+++ fas/CRUD.js
@@ -0,0 +1,27 @@
+const fas=require("fs");
+
+// fas.mkdir("Thapa",(err)=>{
+// console.log(err);
+// });
+
+// fas.writeFile("read.txt","yash varshney",(err)=>{
+//     console.log(err);
+// });
+// fas.appendFile("read.txt","yash varshney",(err)=>{
+//     console.log(err);
+// });
+
+// fas.readFile("read.txt","utf-8",(err,data)=>{
+//     console.log(data);
+// });
+// fas.rename('read.txt',"yash.txt",(err)=>{
+//     console.log(err);
+// });
+// fas.unlink("yash.txt",(ree)=>{
+//     console.log(ree);
+// });
+
+// fas.rmdir("thapa",(ree)=>{
+//     console.log(ree);
+// });
+
--- fas/index.js
+++ fas/index.js
@@ -1,7 +1,15 @@
 const fs=require('fs');
 
-fs.writeFile('read.txt',"today is awesome day",(err)=>{
-    console.log("file is created");
-    console.log(err);
-});
+// fs.writeFile('read.txt',"today is awesome day",(err)=>{
+//     console.log("file is created");
+//     console.log(err);
+// });
 
+// fs.appendFile("read.txt"," yash varshney ",(err)=>{
+//     console.log(err);
+// });
+
+const data=fs.readFile('read.txt','utf-8',(err,data)=>{
+    console.log(data);
+});
+// console.log(data);
