6a51fc38bc0c7df063154c954409555a3663c86f
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-08-27 21:22:35 +0530

    creating the api by the node js

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,13 +1,25 @@
 const http = require('http');
-
+const fs = require('fs');
+const { json } = require('stream/consumers');
 const server = http.createServer((req, res) => {
-    if(req.url=="/")
-    res.end("hello from the others side thapa");
-    else if(req.url=="/yash")
-    res.end("yash varshney")
-    else
-    {
-        res.writeHead(404,{"content-type":"text/html"});
+    const data = fs.readFileSync(`${__dirname}/userApi/userApi.json`, "utf-8");
+    const orgdata = JSON.parse(data);
+    if (req.url == "/")
+        res.end("hello from the others side thapa");
+    else if (req.url == "/yash") {
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(data)
+    }
+    else if (req.url == "/userapi") {
+        fs.readFile(`${__dirname}/userApi/userApi.json`, "utf-8", (err, data) => {
+            // console.log(data);
+            res.writeHead(200, { "content-type": "application/json" })
+            res.end(orgdata[3].name)
+        });
+        // res.end("api done")
+    }
+    else {
+        res.writeHead(404, { "content-type": "text/html" });
         res.end("<h1>404 erhhuhuhror</h1>")
     }
 });
